37ccb6afcb8ec9c2488e36ebeb78b17a723e7691
UnderDev <scottcoyne@hotmail.com>
2017-03-08 15:52:34 +0000

    Added Camera Streaming In App

--- GUI_Ev3_Myo_Robot/MainPage.xaml.cs
+++ GUI_Ev3_Myo_Robot/MainPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using Lego.Ev3.Core;
+﻿using System;
+using Lego.Ev3.Core;
 using Lego.Ev3.WinRT;
+
 using MyoSharp.Communication;
 using MyoSharp.Device;
 using MyoSharp.Exceptions;
 using MyoSharp.Poses;
-using System;
+
 using System.Diagnostics;
 using Windows.ApplicationModel.Core;
 using Windows.Networking;
@@ -14,16 +16,20 @@ using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
+using System.Linq;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Graphics.Imaging;
+using Windows.Media;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.System.Display;
+using Windows.UI.Xaml.Navigation;
+
 
 namespace GUI_Ev3_Myo_Robot
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    ///  
-
     public sealed partial class MainPage : Page
     {
         //Ev3 Vars
@@ -45,10 +51,25 @@ namespace GUI_Ev3_Myo_Robot
         private Pose _CurrentPose;
         private IMyo _MyMyo;
 
+
+        //Camera Stuff
+        // Prevent the screen from sleeping while the camera is running
+        private readonly DisplayRequest _displayRequest = new DisplayRequest();
+
+        // For listening to media property changes
+        private readonly SystemMediaTransportControls _systemMediaControls = SystemMediaTransportControls.GetForCurrentView();
+
+        // MediaCapture and its state variables
+        private MediaCapture _mediaCapture;
+        private bool _isInitialized = false;
+
+
         public MainPage()
         {
             this.InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Required;
         }
+
 
         #region Myo Setup Methods
         private void btnMyo_Click(object sender, RoutedEventArgs e)
@@ -67,53 +88,35 @@ namespace GUI_Ev3_Myo_Robot
 
             // Start listening 
             _MyoChannel.StartListening();
-
-            //// Create the channel
-            //_myoChannel1 = Channel.Create(ChannelDriver.Create(ChannelBridge.Create(),
-            //                        MyoErrorHandlerDriver.Create(MyoErrorHandlerBridge.Create())));
-
-            //// Create the hub with the channel
-            //_myoHub1 = MyoSharp.Device.Hub.Create(_myoChannel1);
-            //// Create the event handlers for connect and disconnect
-            //_myoHub1.MyoConnected += _myoHub_MyoConnected;
-            //_myoHub1.MyoDisconnected += _myoHub_MyoDisconnected;
-
-            //// Start listening 
-            //_myoChannel1.StartListening();
         }
 
-        private async void _myoHub_MyoDisconnected(object sender, MyoEventArgs e)
+        private void _myoHub_MyoDisconnected(object sender, MyoEventArgs e)
         {
-            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
-            {
-                tblUpdates.Text = tblUpdates.Text + System.Environment.NewLine +
-                                    "Myo disconnected";
-            });
+            UpdateUi("Myo disconnected");
+
             _MyoHub.MyoConnected -= _myoHub_MyoConnected;
             _MyoHub.MyoDisconnected -= _myoHub_MyoDisconnected;
         }
 
-        private async void _myoHub_MyoConnected(object sender, MyoEventArgs e)
+        private void _myoHub_MyoConnected(object sender, MyoEventArgs e)
         {
             _MyMyo = e.Myo;
             e.Myo.Vibrate(VibrationType.Long);
-            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                tblUpdates.Text = "Myo Connected: " + e.Myo.Handle;
-            });
-            // add the pose changed event here
+
+            // Add the pose changed event here
             e.Myo.PoseChanged += Myo_PoseChanged;
 
-            // unlock the Myo so that it doesn't keep locking between our poses
+            // Unlock the Myo so that it doesn't keep locking between our poses
             e.Myo.Unlock(UnlockType.Hold);
 
-            //TbCurrentPose.Text = "Connecting to Robot Please Wait....";
+            UpdateUi("Myo Connected: " + e.Myo.Handle);
 
+            //Find The Bricks Ip Address
             FindHostIP();
         }
         #endregion
 
-        #region Auto Connect Ev3 to Wifi - Needs Fixing
+        #region Auto Connect Ev3 to Wifi - Needs Fixing as performs this every 10 seconds
 
         private async void FindHostIP()
         {
@@ -129,6 +132,7 @@ namespace GUI_Ev3_Myo_Robot
             // Start listen operation.
             try
             {
+                UpdateUi("Finding Ev3 IP Address.. Please Wait");
                 listener.Control.InboundBufferSizeInBytes = _EV3_INBOUND_BUFFER_SIZE;
                 // Don't limit traffic to an address or an adapter.
                 await listener.BindServiceNameAsync(_EV3_PORT);
@@ -140,7 +144,7 @@ namespace GUI_Ev3_Myo_Robot
         }
 
         //Event Fires Off when a message is recived on that Socket
-        async void MessageReceived(DatagramSocket socket, DatagramSocketMessageReceivedEventArgs eventArguments)
+        private async void MessageReceived(DatagramSocket socket, DatagramSocketMessageReceivedEventArgs eventArguments)
         {
             //IF the Robot is not Connected, or maby Retry get connection to the robot 
             //Msg recived every 10 sec
@@ -149,6 +153,8 @@ namespace GUI_Ev3_Myo_Robot
                 IOutputStream outputStream = await socket.GetOutputStreamAsync(
                     eventArguments.RemoteAddress,
                     eventArguments.RemotePort);
+
+                UpdateUi("Found Ev3! Trying To Conecting!");
 
                 BrickInit(eventArguments.RemoteAddress);
             }
@@ -164,28 +170,31 @@ namespace GUI_Ev3_Myo_Robot
         //Gets the current Pose From the user and fires off the Command associated
         private async void Myo_PoseChanged(object sender, PoseEventArgs e)
         {
-            Pose curr = e.Pose;
+            Pose curPose = e.Pose;
             await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
             {
                 BitmapImage bitmapImage = null;
+
+                //Path where all the images are stored
                 string imgPath = "ms-appx://GUI_Ev3_Myo_Robot/Assets/";
 
                 //Displays the Current Pose To the Screen
-                tblUpdates.Text = curr.ToString();
+                tblUpdates.Text = curPose.ToString();
 
                 //Sets the _currentPose to the current pose
-                _CurrentPose = curr;
+                _CurrentPose = curPose;
 
                 //Initilize the Robot
-                if (curr == Pose.FingersSpread)
-                    BrickInit();
+                //if (curr == Pose.FingersSpread)
+                    //BrickInit();
 
                 if (_IsRobotRunning)
                 {
-                    switch (curr)
+                    switch (curPose)
                     {
                         case Pose.Rest:
                             RobotStop();
+                            bitmapImage = new BitmapImage(new Uri(imgPath + "Rest.png"));
                             break;
 
                         case Pose.Fist:
@@ -214,8 +223,7 @@ namespace GUI_Ev3_Myo_Robot
                             break;
 
                         case Pose.Unknown:
-                            break;
-                        default:
+                            bitmapImage = new BitmapImage(new Uri(imgPath + "Unknown.png"));
                             break;
                     }
                     ImgCurPose.Source = bitmapImage;
@@ -227,8 +235,6 @@ namespace GUI_Ev3_Myo_Robot
         #region Brick Setup
         private void BrickInit(HostName RemoteAddress)
         {
-            //TbCurrentPose.Text += "\n Magic Fingers";
-
             //if (_IsRobotRunning == false && _IsRobotConnected == true)
             if (_IsRobotRunning == false && _CountStartUp == 0)
             {
@@ -236,17 +242,17 @@ namespace GUI_Ev3_Myo_Robot
 
                 _brick.BrickChanged += _brick_BrickChanged;
 
-                connectToBrick();//Connects to the brick
+                connectToBrick(RemoteAddress.CanonicalName);//Connects to the brick
 
                 _CountStartUp++;
             }
         }
 
         //Connect to the Lego Brick
-        private async void connectToBrick()
+        private async void connectToBrick(String hostIp)
         {
             await _brick.ConnectAsync();
-
+            UpdateUi("Connected To Ev3 At Address: "+ hostIp);
             // await _brick.DirectCommand.PlayToneAsync(5, 2000, 3000);
 
             //Return True when connected
@@ -364,7 +370,7 @@ namespace GUI_Ev3_Myo_Robot
             await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
             () =>
             {
-                tbRawValue.Text = message;
+                tblUpdates.Text = message;
             }
             );
         }
@@ -373,35 +379,120 @@ namespace GUI_Ev3_Myo_Robot
 
 
 
-        #region Sample Command Methods - ** Not Part of the program **
-
-        //Direct Command  
-        private async void DirectComands()
+        #region Constructor, lifecycle and navigation
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
-            await _brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.A, 60, 3000, true);
-            await _brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.A | OutputPort.B, 60, 3000, true);
-            // Motor in Port A At 60% Power Constant
-            //await _brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.A, 60);
+            await InitializeCameraAsync();
+        }
+        #endregion Constructor, lifecycle and navigation
 
-            // Two Motors at 60% Power
-            //await _brick.DirectCommand.TurnMotorAtPowerAsync(OutputPort.A | OutputPort.B, 60);
+        #region MediaCapture methods
+
+        /// <summary>
+        /// Initializes the MediaCapture, registers events, gets camera device information for mirroring and rotating, and starts preview
+        /// </summary>
+        /// <returns></returns>
+        private async Task InitializeCameraAsync()
+        {
+            Debug.WriteLine("InitializeCameraAsync");
+
+            if (_mediaCapture == null)
+            {
+                UpdateUi("Finding Camera");
+                // Attempt to get the back camera if one is available, but use any camera device if not
+                var cameraDevice = await FindCameraDeviceByPanelAsync(Windows.Devices.Enumeration.Panel.Top);
+
+                if (cameraDevice == null)
+                {
+                    UpdateUi("No camera device found!");
+                    return;
+                }
+
+                // Create MediaCapture and its settings
+                _mediaCapture = new MediaCapture();
+
+                var settings = new MediaCaptureInitializationSettings { VideoDeviceId = cameraDevice.Id };
+
+                // Initialize MediaCapture
+                try
+                {
+                    await _mediaCapture.InitializeAsync(settings);
+                    _isInitialized = true;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    UpdateUi("The app was denied access to the camera");
+                }
+
+                // If initialization succeeded, start the preview
+                if (_isInitialized)
+                {
+                    await StartPreviewAsync();
+                }
+            }
         }
 
-        // Batch Command
-        private async void BatchComands()
+        /// <summary>
+        /// Starts the preview and adjusts it for for rotation and mirroring after making a request to keep the screen on and unlocks the UI
+        /// </summary>
+        /// <returns></returns>
+        private async Task StartPreviewAsync()
         {
-            _brick.BatchCommand.TurnMotorAtPower(OutputPort.A, 60);
-            await _brick.BatchCommand.SendCommandAsync();
+            UpdateUi("Starting Camera Preview");
+
+            // Prevent the device from sleeping while the preview is running
+            _displayRequest.RequestActive();
+
+            // Set the preview source in the UI and mirror it if necessary
+            PreviewControl.Source = _mediaCapture;
+
+            // Start the preview
+            await _mediaCapture.StartPreviewAsync();
         }
 
-        //Sends Sounds/Images to the Block
-        private async void SendData()
+        #endregion MediaCapture methods
+
+        #region Helper functions
+
+        /// <summary>
+        /// Queries the available video capture devices to try and find one mounted on the desired panel
+        /// </summary>
+        /// <param name="desiredPanel">The panel on the device that the desired camera is mounted on</param>
+        /// <returns>A DeviceInformation instance with a reference to the camera mounted on the desired panel if available,
+        ///          any other camera if not, or null if no camera is available.</returns>
+        private static async Task<DeviceInformation> FindCameraDeviceByPanelAsync(Windows.Devices.Enumeration.Panel desiredPanel)
         {
-            await _brick.SystemCommand.CopyFileAsync("test.rdf", "../prjs/myApp/Test1.rsf");
-            await _brick.DirectCommand.PlaySoundAsync(100, "../prjs/myApp/Test1");
+            // Get available devices for capturing pictures
+            var allVideoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+
+            // Get the desired camera by panel
+            DeviceInformation desiredDevice = allVideoDevices.FirstOrDefault(x => x.EnclosureLocation != null && x.EnclosureLocation.Panel == desiredPanel);
+
+            // If there is no device mounted on the desired panel, return the first device found
+            return desiredDevice ?? allVideoDevices.FirstOrDefault();
         }
 
+        /// <summary>
+        /// Saves a SoftwareBitmap to the specified StorageFile
+        /// </summary>
+        /// <param name="bitmap">SoftwareBitmap to save</param>
+        /// <param name="file">Target StorageFile to save to</param>
+        /// <returns></returns>
+        private static async Task SaveSoftwareBitmapAsync(SoftwareBitmap bitmap, StorageFile file)
+        {
+            using (var outputStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, outputStream);
 
-        #endregion
+                // Grab the data from the SoftwareBitmap
+                encoder.SetSoftwareBitmap(bitmap);
+                await encoder.FlushAsync();
+            }
+        }
+
+        #endregion Helper functions 
+
+
+
     }
 }
